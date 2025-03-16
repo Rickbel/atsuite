fa088b73b08bb6579ef82127d8a2e0df8d72f1b9
Rickbel <rickbel_94@outlook.com>
2025-03-15 23:48:20 -0400

    asserts update

---
+++ tests/assert.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('playing with assertions', async ({ page }) => {
+  await page.goto('http://uitestingplayground.com/textinput');
+  
+  //steps
+  //select input textbox is visible
+  await expect(page.locator('#newButtonName')).toBeVisible();
+  //select input and type text
+  await page.locator('#newButtonName').fill('Playwright');
+  //click button
+  await page.locator('#updatingButton').click();
+  //verify text button changed
+  await expect(page.locator('#updatingButton')).toHaveText('Playwright');
+
+
+
+
+});
--- tests/uitesting.spec.js
+++ tests/uitesting.spec.js
@@ -2,9 +2,13 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://uitestingplayground.com/');
-  await page.getByText('Aristotle').click();
-  await page.getByRole('link', { name: 'Resources' }).click();
-  await page.getByRole('link', { name: 'Home' }).click();
-  await page.getByRole('link', { name: 'Click' }).click();
-  await page.getByRole('button', { name: 'Button That Ignores DOM Click' }).click();
+  await page.locator('#title'); //id
+  await page.locator('.alert'); //clase
+  await page.locator('a.navbar-brand').click(); //etiqueta
+  //await page.getByText('Aristotle').click();
+  //await page.locator('text=Aristotle').click();
+  //await page.getByRole('link', { name: 'Resources' }).click();
+  //await page.getByRole('link', { name: 'Home' }).click();
+  //await page.getByRole('link', { name: 'Click' }).click();
+  //await page.getByRole('button', { name: 'Button That Ignores DOM Click' }).click();
 });
